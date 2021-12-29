87c8932d9833a28584af73b71a6d2201fa6267b3
Taiwo <famaktaiwo7@gmail.com>
2021-12-29 09:15:00 +0100

    first commit

---
+++ RealLifeEntityApp/Phone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RealLifeEntityApp
+{
+    public class Phone
+    {
+        public string Brand { get; set; }
+        public  string Model { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public string RAM { get; set; }
+        public string ROM { get; set; }
+
+        //methods
+        public string GetPhoneType()
+        {
+            return Brand + " " + Model;
+        }
+
+        private string GetPhoneMemory()
+        {
+            return ROM;
+        }
+
+        public string GetPhoneSpace()
+        {
+            return RAM;
+        }
+
+        public string Print()
+        {
+                return "I use a " + Color + " " + GetPhoneType() + " which is " + Size + " and has a RAM of " + RAM + " with "  + GetPhoneSpace() + " storage space.";
+        }
+    }
+}
---
+++ RealLifeEntityApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RealLifeEntityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Phone phone = new Phone
+            {
+                Brand = "iPhone",
+                Model = "XR",
+                Color = "Black",
+                Size = "15 inches",
+                RAM = "8gb",
+                ROM = "64gb",
+        };
+
+            Console.WriteLine(phone.Print());
+        }
+    }
+}
